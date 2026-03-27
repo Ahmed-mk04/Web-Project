05a3c71417fcf747c25654812a9a9cbc134fc1c8
Ahmed-mk04 <ahmidouvn@gmail.com>
2026-03-27 18:36:34 +0100

    Ajout creation admin automatique

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('../models/User'); // Import du modèle User pour la création auto
 
 const connectDB = async () => {
     try {
@@ -8,6 +9,26 @@ const connectDB = async () => {
         });
 
         console.log(`✅ MongoDB connecté: ${conn.connection.host}`);
+
+        // ========= CRÉATION AUTO DE L'ADMIN AU DÉMARRAGE =========
+        const adminEmail = 'admin2026@gmail.com';
+        const adminExists = await User.findOne({ email: adminEmail });
+
+        if (!adminExists) {
+            console.log(`⚠️ Aucun admin détecté. Création du compte admin par défaut...`);
+            const nouveauAdmin = new User({
+                firstName: 'Admin',
+                lastName: 'Principal',
+                email: adminEmail,
+                password: 'admin2026',
+                role: 'admin',
+                isActive: true
+            });
+            await nouveauAdmin.save();
+            console.log(`✅ Admin par défaut créé avec succès (${adminEmail}) !`);
+        }
+        // ==========================================================
+
     } catch (error) {
         console.error(`❌ Erreur de connexion MongoDB: ${error.message}`);
         process.exit(1);
